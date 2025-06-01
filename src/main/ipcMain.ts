02595ba3d0c7a7490d0cf7b46ceac0a3b2b34084
defautluser0 <defautluser0@defautluser0.xyz>
2025-06-01 21:44:31 +0200

    !nx

--- src/main/ipcMain.ts
+++ src/main/ipcMain.ts
@@ -119,7 +119,7 @@ export function initIpc(mainWindow: BrowserWindow) {
 }
 
 ipcMain.handle(IpcEvents.OPEN_MONACO_EDITOR, async () => {
-    const title = "Nexulien QuickCSS Editor";
+    const title = "Not-Nexulien QuickCSS Editor";
     const existingWindow = BrowserWindow.getAllWindows().find(w => w.title === title);
     if (existingWindow && !existingWindow.isDestroyed()) {
         existingWindow.focus();
