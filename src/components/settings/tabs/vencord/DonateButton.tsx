d1d5cdf950f175a330a49be4ab7fba8a71248764
defautluser0 <defautluser0@defautluser0.xyz>
2025-07-24 21:47:00 +0200

    22.04 mayb??

--- src/components/settings/tabs/vencord/DonateButton.tsx
+++ src/components/settings/tabs/vencord/DonateButton.tsx
@@ -7,10 +7,10 @@
 import DonateButton from "@components/settings/DonateButton";
 import { DONOR_ROLE_ID, VENCORD_GUILD_ID } from "@utils/constants";
 import { Button, GuildMemberStore } from "@webpack/common";
-import BadgeAPI from "plugins/_api/badges";
+import BadgeAPIPlugin from "plugins/_api/badges";
 
 export const isDonor = (userId: string) => !!(
-    BadgeAPI.getDonorBadges(userId)?.length > 0
+    BadgeAPIPlugin.getDonorBadges(userId)?.length > 0
     || GuildMemberStore.getMember(VENCORD_GUILD_ID, userId)?.roles.includes(DONOR_ROLE_ID)
 );
 
